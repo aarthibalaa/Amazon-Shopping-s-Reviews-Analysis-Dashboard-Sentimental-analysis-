b7d7ff1717bc04eb936ee88c0e4c0651a63fd815
Aarthibala R <162883357+aarthibalaa@users.noreply.github.com>
2026-06-21 21:21:51 +0530

    Update Sentimental analysis.py

--- Sentimental analysis.py
+++ Sentimental analysis.py
@@ -1,21 +1,12 @@
 #importing libraries
 import pandas as pd
-import numpy as np
-import re
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use('ggplot')
-from textblob import TextBlob
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
+import numpy as np 
+import re 
+from textblob import TextBlob 
+from nltk.tokenize import word_tokenize 
+from nltk.stem import PorterStemmer 
 from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english'))
-from wordcloud import WordCloud
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+stop_words = set(stopwords.words('english')) 
 
 # Load data
 df = pd.read_excel(r'C:\Users\user\Downloads\amazon_review_test.xlsx', engine='openpyxl')
